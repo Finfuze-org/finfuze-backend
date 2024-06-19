29586e40e793b9cb339a72047b5ca80cc6a47cc8
Iupac120 <oforokpara@gmail.com>
2024-06-19 16:03:36 +0100

    added stripe

--- src/controllers/transactionController.js
+++ src/controllers/transactionController.js
@@ -1,5 +1,7 @@
 const {generateAccountNo} = require("../utils/accountNo")
+const {addCard} = require("./bankController")
 const pool = require("../config/connect")
+const stripe = require("stripe")(process.env.STRIPE_KEYS)
 
 
 const createAccountNo = async(req,res) => {
@@ -16,38 +18,81 @@ const createAccountNo = async(req,res) => {
 const sendFinzuze = async (req,res) => {
     const {receiverAcc, amount, paymentMethod} = req.body
     const userId = req.user.user_id
-    try {
-        if(paymentMethod === "bank"){
-            await pool.query("")
+
+        try {
+            await pool.query("BEGIN")
+            if(paymentMethod === "bank"){
+                const cardExist = await pool.query("SELECT account_no,bank_name FROM cards WHERE user_id = $1",[userId])
+    
+                if(cardExist.rows.length === 0) {
+                    await pool.query("ROLLBACK")
+                    res.status(404).json("card does not exist")
+                    }
+                    //const addNewCard = addCard(req,res)
+                    // if(addNewCard.error){
+                    //     await pool.query("ROLLBACK")
+                    //     res.status(4040).json(addNewCard.error)
+                    // }
+
+                const paymentIntent = await stripe.paymentIntents.create({
+                    amount:amount*100,
+                    currency:"usd",
+                    payment_method_types:["card"],
+                    // payment_method_data:{
+                    //     type:"card",
+                    //     card:"carddetail"
+                    // },
+                    metadata:{userId,receiverAcc},
+                    confirm:true
+                })
+                if(paymentIntent.status !== "succeded"){
+                    await pool.query("ROLLBACK")
+                    res.status(404).json("payment failed")
+                }
+                const receiverExist = await pool.query("SELECT * FROM finfuzeAccount WHERE account_no = $1",[receiverAcc])
+                if(receiverExist.rows.length === 0){
+                    await pool.query("ROLLBACK")
+                    res.status(404).json("receiver account not found")
+                }
+                await pool.query("UPDATE finfuzeAccount SET balance = balance + $1 WHERE account_no = $2",[amount,receiverAcc])
+            }
+            
+            else if(paymentMethod === "finfuze"){
+                const userIdAcc = await pool.query("SELECT * FROM finfuzeAccount WHERE user_id = $1",[userId])
+                const sendIdAcc = await pool.query("SELECT * FROM finfuzeAccount WHERE account_no =$1",[receiverAcc])
+                if(!userIdAcc.rows.length === 0 || !sendIdAcc.rows.length === 0) {
+                    await pool.query("ROLLBACK");
+                    return res.status(404).json("User not found")
+                }
+                const user = userIdAcc.rows[0]
+                const receiver = sendIdAcc.rows[0]
+                if(user.balance < amount) {
+                    await pool.query("ROLLBACK");
+                    return res.status(401).json("insufficient balance")
+                }
+                const acountWith = user.balance -= amount
+                await pool.query("UPDATE finfuzeAccount SET balance = $1 WHERE user_id =$2",[acountWith,userId])
+                await pool.query("UPDATE finfuzeAccount SET balance = balance + $1 WHERE account_no = $2",[amount,receiverAcc])
+            }else{
+                await pool.query("ROLLBACK")
+                res.status(404).json({data:error.message})
+            }
+    
+    
+            const transaction = await pool.query("INSERT INTO transaction_history (amount,transact_status,transact_type,payment_method,receiver_id,user_id) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *",[amount,'Pending','expense',paymentMethod,receiverAcc,userId])
+            if(!transaction.rowCount === 0) {
+                await pool.query("ROLLBACK");
+                return res.status(404).json("transaction failed")
+            }
+            await pool.query("COMMIT")
+            res.status(201).json("transaction successful")  
+        } catch (error) {
+            await pool.query("ROLLBACK")
+            res.status(404).json(error.message)
         }
-        await pool.query("BEGIN")
-        const userIdAcc = await pool.query("SELECT * FROM finfuzeAccount WHERE user_id = $1",[userId])
-        const sendIdAcc = await pool.query("SELECT * FROM finfuzeAccount WHERE account_no =$1",[receiverAcc])
-        if(!userIdAcc.rows.length === 0 || !sendIdAcc.rows.length === 0) {
-            await pool.query("ROLLBACK");
-            return res.status(404).json("User not found")
-        }
-        const user = userIdAcc.rows[0]
-        const receiver = sendIdAcc.rows[0]
-        if(user.balance < amount) {
-            await pool.query("ROLLBACK");
-            return res.status(401).json("insufficient balance")
-        }
-        const acountWith = user.balance -= amount
-        await pool.query("UPDATE finfuzeAccount SET balance = $1 WHERE user_id =$2",[acountWith,userId])
-        await pool.query("UPDATE finfuzeAccount SET balance = balance + $1 WHERE account_no = $2",[amount,receiverAcc])
-        const transaction = await pool.query("INSERT INTO transaction_history (amount,transact_status,transact_type,payment_method,receiver_id,user_id) VALUES ($1,$2,$3,$4,$5,$6) RETURNING *",[amount,'Pending','expense',paymentMethod,receiverAcc,userId])
-        if(!transaction.rowCount === 0) {
-            await pool.query("ROLLBACK");
-            return res.status(404).json("transaction failed")
-        }
-        await pool.query("COMMIT")
-        res.status(201).json("transaction successful") 
-    } catch (error) {
-        await pool.query("ROLLBACK")
-        res.status(404).json({data:error.message})
     }
-}
+
+
 
 const transact = async(req,res) => {
     const {userId} = req.body
