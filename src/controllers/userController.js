4a527516cd43db5e12da1938207e095c5581fed4
Iupac120 <oforokpara@gmail.com>
2024-07-02 13:51:57 +0100

    updated user profile

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,7 +2,9 @@ const bcrypt = require("bcrypt")
 const cloudinary = require("../config/cloudinary")
 
 const updateUser = async (req,res) => {
+    console.log("here")
     const {email,password,firstName,lastName,middleName,birthDate,contactInfo,contactAddress} = req.body
+   console.log("here")
     await cloudinary.UploadStream.upload(req.file.path,{resource_type:"image"},async function(err,result){
         if(err){
             console.log(err)
@@ -11,15 +13,16 @@ const updateUser = async (req,res) => {
                 msg:"error in uploading file"
             })
         }
-        const {url, public_id} = result
-    })
+        const {url, public_id} = result;
+        console.log("here",url,public_id)
     const emailExist = await pool.query("SELECT * FROM person WHERE user_email = $1",[email])
     if(emailExist.rows.length === 0) return res.status(404).json("Email does not exist")
-    const isMatch = await bcrypt.compare(emailExist.ser_password,password)
+    const isMatch = await bcrypt.compare(emailExist.user_password,password)
     if(!isMatch) return res.status(404).json("password iscorrect")
     const updateUser = await pool.query("UPDATE person SET first_name,last_name,middle_name,birth_date,avatar_url,contact_info,contact_address,user_email VALUES ($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *",[firstName,lastName,middleName,birthDate,url,contactInfo,contactAddress,email])
     if(updateUser.rows.length === 0) return res.status(404).json("Failed to update user")
     res.status(201).json({data:updateUser.rows[0]})
+    })
 }
 
 module.exports = {
